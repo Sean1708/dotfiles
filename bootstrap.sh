70cc4d3a1dfad3af45807c1d16500307ceea9db0
Sean Marshallsay <srm.1708@gmail.com>
2014-12-20 12:27:04 +0000

    Stow global .gitignore.

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,4 +15,18 @@ do
             echo 'ok!'
         fi
     fi
+
+    # lookup if this can be done with stow
+    # stow ignores .gitignore annoyingly
+    echo -n "Stowing .gitignore... "
+
+    rm -f "${HOME}/.gitignore"
+    ln -s git/.gitignore "${HOME}"
+    
+    if [ $? -ne 0 ]
+    then
+        echo 'failed.'
+    else
+        echo 'ok!'
+    fi
 done
