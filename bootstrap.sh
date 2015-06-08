4ef699040f7ce575ed6c73ea2dab3361038b4740
Sean Marshallsay <srm.1708@gmail.com>
2015-06-08 07:43:05 +0000

    Reduce indent on shell scripts.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,23 +2,23 @@
 
 for pkg in [^.]*
 do
-    if [ "$pkg" != 'bootstrap.sh' -a "$pkg" != 'README.md' ]
+  if [[ "$pkg" != 'bootstrap.sh' -a "$pkg" != 'README.md' ]]
+  then
+    echo -n "Stowing ${pkg}... "
+
+    case "$pkg" in
+      *vim | ipython | bash)
+        opts='--no-folding'
+        ;;
+      *)
+        unset opts
+        ;;
+    esac
+    stow --target="$HOME" $opts "$pkg"
+
+    if [[ $? -eq 0 ]]
     then
-        echo -n "Stowing ${pkg}... "
-
-        case "$pkg" in
-            *vim | ipython | bash)
-                opts='--no-folding'
-                ;;
-            *)
-                unset opts
-                ;;
-        esac
-        stow --target="$HOME" $opts "$pkg"
-
-        if [ $? -eq 0 ]
-        then
-            echo 'ok!'
-        fi
+      echo 'ok!'
     fi
+  fi
 done
