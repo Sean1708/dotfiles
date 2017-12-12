2986c70a54b43e8ae971dc4fda8b27e2475fe8e9
Sean Marshallsay <srm.1708@gmail.com>
2017-12-12 18:32:04 +0000

    bootstrap: Allow symlinking existing files.

--- bootstrap.py
+++ bootstrap.py
@@ -3,15 +3,14 @@
 import os
 import sys
 
-def main(packages):
-    if len(packages) <= 0:
-        packages = filter(
-            lambda path: os.path.isdir(path) and not path.startswith("."),
-            os.listdir(),
-        )
 
-    for package in packages:
-        link(package)
+def symlink(src, dest):
+    try:
+        os.symlink(src, dest)
+    except FileExistsError:
+        os.remove(dest)
+        os.symlink(src, dest)
+
 
 def link(package):
     for root, _, files in os.walk(package):
@@ -27,10 +26,22 @@ def link(package):
         os.makedirs(dir, exist_ok=True)
 
         for file in files:
-            os.symlink(
+            symlink(
                 os.path.abspath(os.path.join(root, file)),
                 os.path.join(dir, file),
             )
+
+
+def main(packages):
+    if len(packages) <= 0:
+        packages = filter(
+            lambda path: os.path.isdir(path) and not path.startswith("."),
+            os.listdir(),
+        )
+
+    for package in packages:
+        link(package)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
