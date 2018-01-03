19c4a76a4d6c94f4a56417d0e1644a942b36ba4d
Sean Marshallsay <srm.1708@gmail.com>
2018-01-03 22:38:40 +0000

    awesome: Improve battery code.

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -93,6 +93,11 @@ local function client_menu_toggle_fn()
         end
     end
 end
+
+local function trim(s)
+	-- NOTE: Example `trim6` from `https://lua-users.org/wiki/StringTrim`.
+	return s:find("^%s*$") and "" or s:match("^%s*(.*%S)")
+end
 -- }}}
 
 -- {{{ Menu
@@ -125,92 +130,292 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 -- NOTE: Spaces around the string are far more visually pleasing.
 -- TODO: Can the spaces be replaced by padding?
 mytextclock = wibox.widget.textclock(" %a %d %b, %H:%M ")
+-- TODO: Have 1 & 3 change month and 2 close it.
+mycalendar = awful.widget.calendar_popup.month()
+mycalendar:attach(mytextclock, "tr")
 
--- TODO: Arbitrary number of batteries.
+-- TODO: Move into a seperate file.
 -- TODO: Notification on low battery.
--- TODO: There must be a nicer way to do this. Learn LUA!!!
 -- TODO: Open ticket about documentation not working.
--- TODO: Have a single battery bar that pops-up with all individual batteries when clicked (should weight batteries and give extra info in the pop-up, progress bar should just be a battery symbol actual percentage should show up in the extra info).
-local bat0pb = wibox.widget {
-	widget = wibox.widget.progressbar,
-	min_value = 0,
-	max_value = 100,
-	value = 100,
-	forced_width = 50,
-	paddings = 1,
-}
+local battery
+do
+	local function info(supply, variable)
+		local dir = string.format('/sys/class/power_supply/%s/', supply)
+		return string.lower(trim(assert(io.open(dir .. variable)):read('a')))
+	end
 
-local bat0tb = wibox.widget {
-	widget = wibox.widget.textbox,
-	text = "100%",
-	align = "center",
-}
+	local batteries = {}
+	local n = 0
+	local total_capacity = 0
+	for supply in assert(io.popen('ls /sys/class/power_supply')):lines() do
+		if info(supply, 'type') == 'battery' then
+			local capacity = tonumber(info(supply, 'energy_full'))
+			batteries[supply] = {
+				manufacturer = info(supply, 'manufacturer'),
+				model = info(supply, 'model_name'),
+				capacity = capacity,
+			}
+			total_capacity = total_capacity + capacity
+			n = n + 1
+		end
+	end
+	setmetatable(batteries, { __len = function () return n end })
 
-local bat0 = wibox.widget {
-	bat0pb,
-	bat0tb,
-	layout = wibox.layout.stack,
-	set_battery = function (self, val)
-		bat0pb.value = tonumber(val)
-		bat0tb.text = tostring(val) .. "%"
-	end,
-}
+	local progress = wibox.widget {
+		widget = wibox.widget.progressbar,
 
-local bat1pb = wibox.widget {
-	widget = wibox.widget.progressbar,
-	min_value = 0,
-	max_value = 100,
-	value = 100,
-	forced_width = 50,
-	paddings = 1,
-}
+		min_value = 0,
+		max_value = 100,
+		value = nil,
 
-local bat1tb = wibox.widget {
-	widget = wibox.widget.textbox,
-	text = "100%",
-	align = "center",
-}
+		forced_width = 50,
+		paddings = 1,
+	}
 
-local bat1 = wibox.widget {
-	bat1pb,
-	bat1tb,
-	layout = wibox.layout.stack,
-	set_battery = function (self, val)
-		bat1pb.value = tonumber(val)
-		bat1tb.text = tostring(val) .. "%"
-	end,
-}
+	function progress:update(charge)
+		local colour
+		if charge < 20 then
+			colour = '#ff0000'  -- Red
+		elseif charge < 50 then
+			colour = '#ffff00'  -- Yellow
+		else
+			colour = '#00ff00'  -- Green
+		end
 
-mybatterybar = wibox.widget {
-	bat0,
-	bat1,
-	layout = wibox.layout.fixed.horizontal,
-	update_battery = function (val0, val1)
-		bat0.battery = val0
-		bat1.battery = val1
-	end,
-}
+		self.value = charge
+		self.color = colour
+	end
 
-local function set_battery()
-	local bat0_file = assert(io.open("/sys/class/power_supply/BAT0/capacity", "r"))
-	local bat1_file = assert(io.open("/sys/class/power_supply/BAT1/capacity", "r"))
-	
-	-- Using `tonumber` strips extraneous whitespace.
-	local bat0 = tonumber(bat0_file:read("*all"))
-	local bat1 = tonumber(bat1_file:read("*all"))
+	-- TODO: Replace this with icons.
+	local percent = wibox.widget {
+		widget = wibox.widget.textbox,
 
-	mybatterybar.update_battery(bat0, bat1)
+		text = nil,
+		align = "center",
+	}
+
+	function percent:update(charge)
+		self.text = string.format('%.1f', charge) .. '%'
+	end
+
+	local bar = wibox.widget {
+		progress,
+		percent,
+
+		layout = wibox.layout.stack,
+	}
+	bar.progress = progress
+	bar.percent = percent
+
+	function bar:update(charge)
+		self.progress:update(charge)
+		self.percent:update(charge)
+	end
+
+	local columns = {}
+	for id, info in pairs(batteries) do
+		local header = wibox.widget {
+			widget = wibox.widget.textbox,
+
+			text = string.format('%s: %s(%s)', id, string.upper(info.manufacturer), info.model),
+		}
+
+		local status = wibox.widget {
+			widget = wibox.widget.textbox,
+
+			text = nil,
+		}
+
+		function status:update(status)
+			self.text = string.upper(status)
+		end
+
+		local progress = wibox.widget {
+			widget = wibox.widget.progressbar,
+
+			min_value = 0,
+			max_value = 100,
+			value = nil,
+
+			-- NOTE: Changing the value of `forced_width` doesn't seem to have any effect, but omitting it makes things fucky.
+			forced_width = 50,
+			forced_height = 1.5 * beautiful.get_font_height(),
+			paddings = 1,
+		}
+
+		function progress:update(charge)
+			local colour
+			if charge < 20 then
+				colour = '#ff0000'  -- Red
+			elseif charge < 50 then
+				colour = '#ffff00'  -- Yellow
+			else
+				colour = '#00ff00'  -- Green
+			end
+
+			self.value = charge
+			self.color = colour
+		end
+
+		local percent = wibox.widget {
+			widget = wibox.widget.textbox,
+
+			text = nil,
+
+			align = "center",
+		}
+
+		function percent:update(charge)
+			self.text = string.format('%.1f', charge) .. '%'
+		end
+
+		local charge = wibox.widget {
+			progress,
+			percent,
+
+			layout = wibox.layout.stack,
+		}
+		charge.progress = progress
+		charge.percent = percent
+
+		function charge:update(charge)
+			self.progress:update(charge)
+			self.percent:update(charge)
+		end
+
+		-- TODO: Make this an actual button that does stuff.
+		local button = wibox.widget {
+			widget = wibox.widget.textbox,
+
+			text = "Calibrate...",
+		}
+
+		local column = wibox.widget {
+			header,
+			status,
+			charge,
+			button,
+
+			layout = wibox.layout.fixed.vertical,
+			spacing = 5,
+		}
+		column.id = id
+		column.header = header
+		column.status = status
+		column.charge = charge
+		column.button = button
+
+		function column:update(battery)
+			self.charge:update(battery.charge)
+			self.status:update(battery.status)
+		end
+
+		table.insert(columns, column)
+	end
+
+	table.sort(columns, function (l, r) return l.id < r.id end)
+
+	function columns:update(batteries)
+		for _, column in ipairs(self) do
+			column:update(batteries[column.id])
+		end
+	end
+
+	local popup = wibox {
+		ontop = true,
+		border_width = 5,
+		widget = wibox.widget(gears.table.join(columns, {
+			layout = wibox.layout.fixed.horizontal,
+			spacing = 10,
+		})),
+	}
+	popup.columns = columns
+
+	function popup:update(batteries)
+		self.columns:update(batteries)
+	end
+
+	function popup:draw()
+		local screen = awful.screen.focused()
+
+		local width, height = wibox.widget.base.fit_widget(
+			self.widget,
+			{ screen = screen, dpi = beautiful.xresources.get_dpi(screen) },
+			self.widget,
+			screen.workarea.width,
+			screen.workarea.height)
+
+		-- NOTE: Subtracting 10 fixes clipping due to border.
+		-- TODO: Fix it in a better way.
+		local x = screen.workarea.x + screen.workarea.width - width - 10
+		local y = screen.workarea.y
+
+		self:geometry {
+			height = height,
+			width = width,
+			x = x,
+			y = y,
+		}
+	end
+
+	function popup:toggle()
+		self:draw()
+		self.visible = not self.visible
+	end
+
+	popup:buttons(gears.table.join(
+		awful.button({ }, 1, function (w) w.visible = false end)))
+
+	battery = {
+		batteries = batteries,
+		capacity = total_capacity,
+		mode = nil,
+		charge = nil,
+
+		-- TODO: When moving this to a new file just use captures, don't worry about putting everything in one table.
+		bar = bar,
+		popup = popup,
+	}
+
+	function battery:update()
+		self.mode = 'mains'
+		local charge = 0
+
+		for id, status in pairs(self.batteries) do
+			status.charge = tonumber(info(id, 'capacity'))
+			status.level = info(id, 'capacity_level')
+			status.status = info(id, 'status')
+
+			if status.status == 'charging' then
+				self.mode = 'charging'
+			elseif status.status == 'discharging' then
+				self.mode = 'battery'
+			end
+
+			charge = charge + status.capacity * status.charge
+		end
+
+		self.charge = charge / self.capacity
+
+		self.bar:update(self.charge)
+		self.popup:update(self.batteries)
+	end
+
+	battery.bar:buttons(gears.table.join(
+		awful.button({ }, 1, function () battery.popup:toggle() end)))
+
+
+	gears.timer {
+		timeout = 30,
+		autostart = true,
+		callback = function () battery:update() end,
+	}
+	battery:update()
 end
-
-gears.timer {
-	timeout = 60,
-	autostart = true,
-	callback = set_battery,
-}
-set_battery()
 
 
 -- TODO: Brightness and volume indicators (progress bar with a symbol overlaid, extra information in pop-up).
+-- TODO: Music bar (with controls).
 
 
 -- Create a wibox for each screen and add it
@@ -316,8 +521,7 @@ awful.screen.connect_for_each_screen(function (s)
 		awful.button({ }, 1, function () awful.layout.inc( 1) end),
 		awful.button({ }, 3, function () awful.layout.inc(-1) end),
 		awful.button({ }, 4, function () awful.layout.inc( 1) end),
-		awful.button({ }, 5, function () awful.layout.inc(-1) end)
-	))
+		awful.button({ }, 5, function () awful.layout.inc(-1) end)))
 	-- Create a taglist widget
 	s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, taglist_buttons)
 
@@ -339,7 +543,7 @@ awful.screen.connect_for_each_screen(function (s)
 		s.mytasklist, -- Middle widget
 		{ -- Right widgets
 			layout = wibox.layout.fixed.horizontal,
-			mybatterybar,
+			battery.bar,
 			wibox.widget.systray(),
 			mytextclock,
 			s.mylayoutbox,
